d08be5f81c45f69f27dc600f9ff5088885078888
codemodel6 <codemodel6@gmail.com>
2024-07-26 12:19:17 +0900

    feat : "main.css 설정 및 homepage 작업중"

---
+++ src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>{{ message }}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      messgae: '흠냐'
+    }
+  }
+}
+</script>
+<style lang=""></style>
