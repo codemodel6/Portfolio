5f4a5cabf0587c617a5c801bc5af33bf994e1517
codemodel6 <codemodel6@gmail.com>
2024-07-26 17:29:32 +0900

    feat : "변수 출력 해보기 완"

--- src/App.vue
+++ src/App.vue
@@ -4,39 +4,10 @@ import HomePage from './views/HomePage.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper"></div>
   </header>
 
-  <HomePage>dd?</HomePage>
+  <HomePage></HomePage>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,10 @@
-<template lang="">
-  <div>{{ message }}</div>
-</template>
-<script>
-export default {
-  data() {
-    return {
-      messgae: '흠냐'
-    }
-  }
-}
+<script setup lang="ts">
+const hi = '1'
 </script>
+
+<template lang="">
+  <div>{{ hi }}</div>
+  <div>음?</div>
+</template>
+
 <style lang=""></style>
