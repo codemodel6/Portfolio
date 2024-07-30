2f1e4d8938007d709bbe1a2d106760998ed75a39
codemodel6@gmail.com <codemodel6@gmail.com>
2024-07-30 23:45:32 +0900

    feat : "Marquee 컴포넌트 분리"

---
+++ src/components/molecule/HomeMarquee.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="marquee">
+    <div>
+      <span>방문을 환영합니다!</span>
+      <span>방문을 환영합니다!</span>
+      <span>방문을 환영합니다!</span>
+      <span>방문을 환영합니다!</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '@/assets/styles/global/GlobalBlock.scss';
+@import '@/assets/styles/color/ColorNote.scss';
+
+.marquee {
+  height: 30px;
+  width: 100%;
+  overflow: hidden;
+  position: relative;
+  border-bottom: 3px solid $Primary100;
+}
+
+.marquee div {
+  display: flex;
+  width: 200%;
+  height: 30px;
+  position: absolute;
+  overflow: hidden;
+  animation: marquee 20s linear infinite;
+}
+
+.marquee span {
+  display: flex;
+  align-items: center;
+  float: left;
+  width: 25%;
+  font-weight: bold;
+  height: 100%;
+}
+
+@keyframes marquee {
+  0% {
+    left: 0;
+  }
+  100% {
+    left: -100%;
+  }
+}
+</style>
