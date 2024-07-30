69bada60b1b6c85b892e0c5fe3a73798d269a8a8
codemodel6 <codemodel6@gmail.com>
2024-07-30 16:26:17 +0900

    design : "헤더 틀 완성"

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheHeader from './components/organism/TheHeader.vue'
+</script>
 
 <template>
   <div id="app">
     <router-view />
+    <TheHeader />
   </div>
-  <header>
-    <div class="wrapper"></div>
-  </header>
 </template>
 
 <style scoped></style>
--- src/components/organism/TheHeader.vue
+++ src/components/organism/TheHeader.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div class="header-wrapper"></div>
 </template>
 
 <style lang="scss">
+@import '../../assets/styles/color/ColorNote.scss';
+
 .header-wrapper {
-  background-color:;
+  background-color: $Sub100;
   z-index: 100;
   position: fixed;
+  width: 100%;
+  border-bottom: 3px solid $Primary100;
+  height: 90px;
 }
 </style>
