97b74bfedc0263ada6a061428b3b34f9899cedec
codemodel6@gmail.com <codemodel6@gmail.com>
2024-08-09 02:58:41 +0900

    feat : "인트로 완성"

--- src/views/IntroPage.vue
+++ src/views/IntroPage.vue
@@ -1,8 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+onMounted(() => {
+  const router = useRouter()
+
+  // 6초 후 홈으로 이동
+  setTimeout(() => {
+    router.push('/home')
+  }, 6000)
+})
+</script>
 
 <template>
   <div class="intro-page-wrapper">
-    <span class="intro-word">C</span>
+    <span class="intro-first-word">C</span>
+    <span class="intro-rest-word">odemodel6</span>
+    <span class="intro-portfolio-word">Portfolio</span>
   </div>
 </template>
 
@@ -13,14 +27,40 @@
   height: 100vh;
   min-width: 100vh;
   width: 100%;
+  background-color: black;
+  color: white;
 
-  .intro-word {
+  .intro-first-word {
     position: absolute;
     top: 50%;
     left: 50%;
-    font-size: 20rem;
+    font-size: 10rem;
     animation: rotate-word 3s ease-out;
     transform: translate(-50%, -50%);
+    animation-fill-mode: forwards;
+  }
+
+  span:nth-child(2) {
+    position: absolute;
+    font-size: 10rem;
+    top: 42%;
+    left: 44%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+    animation: opacity-word 1s ease-in;
+    animation-delay: 3.2s;
+    animation-fill-mode: forwards;
+  }
+
+  span:nth-child(3) {
+    position: absolute;
+    font-size: 10rem;
+    top: 60%;
+    left: 65%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+    animation: opacity-word 1s ease-in;
+    animation-delay: 4.2s;
     animation-fill-mode: forwards;
   }
 }
@@ -30,14 +70,25 @@
     transform: translate(-50%, -50%);
   }
 
-  50% {
+  70% {
+    top: 50%;
     transform: translate(-50%, -50%) rotateY(1080deg);
     left: 50%;
   }
 
   100% {
+    top: 42%;
     transform: translate(-50%, -50%) rotateY(1080deg);
     left: 20%;
+  }
+}
+
+@keyframes opacity-word {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
   }
 }
 </style>
