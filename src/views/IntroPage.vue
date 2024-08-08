8c5a3a1f285a66bdd9e8167fb0e4efe8dee63ac1
codemodel6 <codemodel6@gmail.com>
2024-08-08 17:30:21 +0900

    design : "인트로 애니메이션 작업중"

--- src/views/IntroPage.vue
+++ src/views/IntroPage.vue
@@ -1,27 +1,43 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="intro-page-wrapper"></div>
+  <div class="intro-page-wrapper">
+    <span class="intro-word">C</span>
+  </div>
 </template>
 
 <style lang="scss">
+@import '@/assets/styles/color/ColorNote.scss';
+
 .intro-page-wrapper {
   height: 100vh;
   min-width: 100vh;
   width: 100%;
-  font-size: 30rem;
-  background-color: orange;
+
+  .intro-word {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    font-size: 20rem;
+    animation: rotate-word 3s ease-out;
+    transform: translate(-50%, -50%);
+    animation-fill-mode: forwards;
+  }
 }
 
-@keyframes rotate-and-shrink {
+@keyframes rotate-word {
   0% {
-    transform: rotate(0deg) scale(1);
+    transform: translate(-50%, -50%);
   }
+
   50% {
-    transform: rotate(180deg) scale(0.5) translateY(50px);
+    transform: translate(-50%, -50%) rotateY(1080deg);
+    left: 50%;
   }
+
   100% {
-    transform: rotate(360deg) scale(0.1) translateX(200px);
+    transform: translate(-50%, -50%) rotateY(1080deg);
+    left: 20%;
   }
 }
 </style>
